f911835c65b898f98f352d198cf93f14ddd88091
Timur <minteemer@gmail.com>
2019-11-17 14:01:02 +0300

    Object detection image preprocessing fix

--- app/src/main/java/iu/quaraseequi/erzhan/repositories/objectDetection/ObjectDetectionRepositoryImpl.kt
+++ app/src/main/java/iu/quaraseequi/erzhan/repositories/objectDetection/ObjectDetectionRepositoryImpl.kt
@@ -13,21 +13,20 @@ class ObjectDetectionRepositoryImpl(
 
     companion object {
         const val CROP_SIZE = 300
+    }
 
-        private val frameToCropTransform by lazy {
+    override fun detectObjects(image: Bitmap): List<Classifier.Recognition> {
+        val frameToCropTransform by lazy {
             ImageUtils.getTransformationMatrix(
-                CROP_SIZE, CROP_SIZE,
+                image.width, image.height,
                 CROP_SIZE, CROP_SIZE,
                 90, false
             )
         }
 
-        private val cropToFrameTransform
-                by lazy { Matrix().also { frameToCropTransform.invert(it) } }
-    }
+        val cropToFrameTransform = Matrix().also { frameToCropTransform.invert(it) }
 
-    override fun detectObjects(image: Bitmap): List<Classifier.Recognition> =
-        detector.recognizeImage(image.cropToSize()).apply {
+        return detector.recognizeImage(image.applyTransform(frameToCropTransform)).apply {
             forEach {
                 val location = it.location
                 cropToFrameTransform.mapRect(it.location)
@@ -37,12 +36,13 @@ class ObjectDetectionRepositoryImpl(
                 )
             }
         }
+    }
 
-    private fun Bitmap.cropToSize(): Bitmap {
+    private fun Bitmap.applyTransform(transform: Matrix): Bitmap {
         val croppedBitmap = Bitmap.createBitmap(CROP_SIZE, CROP_SIZE, Bitmap.Config.ARGB_8888)
 
         val canvas = Canvas(croppedBitmap)
-        canvas.drawBitmap(this, frameToCropTransform, null)
+        canvas.drawBitmap(this, transform, null)
 
         return croppedBitmap
     }
